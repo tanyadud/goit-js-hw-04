2aaed6118ae09258ff2914e85a9a0c7110fc0c2e
Tanya Olieinykova <dp210795dtj1@gmail.com>
2024-06-12 00:19:28 +0300

    add 3 tasks

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function isEnoughCapacity(products, containerSize) {
+    let totalCapacity = 0;
+    const arrValueProducts = Object.values(products);
+  
+   for (const product of arrValueProducts) {
+     totalCapacity += product;
+   }
+    return totalCapacity <= containerSize;
+}
+
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); 
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); 
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  );
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  );
---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+"use srict";
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+changeUsername(newName) {
+    this.username = newName;
+},
+  
+updatePlayTime(hours) {
+   this.playTime += hours;
+},
+  
+getInfo() {
+  return `${profile.username} has ${profile.playTime} active hours!`
+},
+
+}
+
+
+console.log(profile.getInfo()); 
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
+
